83c3c1260a1e71b7006e4caf81082e20eceba826
Tushar Pandey <toshu033@gmail.com>
2026-06-02 09:54:42 +0530

    Add ArithmeticOperators class for basic operations

---
+++ src/Operator/Arithmetic.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args){
+        int a = 25;
+        int b = 12;
+
+       System.out.println("a + b");  //(" ") -- it stores String.
+       System.out.println(a+b);  //( ) -- it stores operations.
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
