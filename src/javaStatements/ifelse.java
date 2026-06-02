3adbb5783db93fc861f7fb71c883b79d0c9b1e36
Tushar Pandey <toshu033@gmail.com>
2026-06-03 01:44:55 +0530

    new

---
+++ src/javaStatements/ifelse.java
@@ -0,0 +1,20 @@
+public class ifelse {
+    public static void main(String[] args){
+//        int a = 2;
+//        if(a >= 12){
+//            System.out.println("you can vote");
+//            System.out.println("Hehe!");
+//        }
+//        else{
+//            System.out.println("you can't vote");
+//        }
+
+        int day1 = 1;
+        if(day1 == 23){
+            System.out.println("Go to Office");
+        } else{
+            System.out.println("It's Sunday");
+        }
+
+    }
+}
