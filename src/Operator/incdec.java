796065676939720d68e7c95f4ae4c32e8b2dc226
Tushar Pandey <toshu033@gmail.com>
2026-06-02 09:57:22 +0530

    Add incDecOperators class with increment/decrement example

---
+++ src/Operator/incdec.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class incDecOperators {
+    public static void main(String[] args){
+        int a = 23;
+        a = a+1;
+        a++;
+        a = a-1;
+
+        System.out.println(a);
+    }
+}
