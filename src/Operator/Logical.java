c77398fb5639674443ab425e9c57d36638c0dabd
Tushar Pandey <toshu033@gmail.com>
2026-06-02 09:57:47 +0530

    Create LogicalOperator class for logical operations
    Added LogicalOperator class to demonstrate logical operations.

---
+++ src/Operator/Logical.java
@@ -0,0 +1,43 @@
+package operators;
+
+public class LogicalOperator {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        boolean c = true;
+
+        /*  System.out.println(a && a); // 1 AND 1 = 1
+        System.out.println(a && b); // 1 AND 0 = 0
+        System.out.println(b && b); // 0 AND 0 = 0
+        System.out.println(b && a); // 0 AND 1 = 0
+
+       */
+
+        /*true, only if both epression1
+        and expression2 are true.
+         */
+
+        /*System.out.println(b || b); // 0 or 0 = 0
+        System.out.println(a||b); // 0 or 1 = 1
+        System.out.println(b||a); // 1 or 0 = 1
+        System.out.println(a || b); //1 or 1 = 1
+
+         */
+
+        /*
+        true if either expressio1
+        and expression2 is true
+         */
+
+        System.out.println(!a); // !1 = 0
+        System.out.println(!b); // !0 = 1
+
+        /*
+        true if expression is false and vice versa.
+         */
+
+
+
+    }
+}
