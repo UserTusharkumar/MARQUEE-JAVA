8127aede1b9d31704077bcbbc3a4285f58256b89
Tushar Pandey <toshu033@gmail.com>
2026-06-02 11:50:25 +0530

    new

---
+++ src/array/swaparray.java
@@ -0,0 +1,19 @@
+public class swapArray {
+
+    public void swapElements(int[] arr) {
+        int temp = arr[1];
+        arr[1] = arr[2];
+        arr[2] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+
+        swapArray obj = new swapArray();
+        obj.swapElements(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
