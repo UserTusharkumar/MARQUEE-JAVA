65229b4693804a772c2ac3912c2ae6b8914ccbba
Tushar Pandey <toshu033@gmail.com>
2026-06-02 09:59:43 +0530

    Add datatypes.java with basic data type examples

---
+++ src/datatypes.java
@@ -0,0 +1,17 @@
+package DataTypesss;
+
+public class dataTypes {
+    public static void main(String[] args){
+        boolean isPassed = true;
+        int count = 12;
+
+        byte marks = 13;
+        short studentsCount = 1235;
+
+        float pi = 3.14f;
+        double secondPi = 3.14;
+
+        System.out.printf("%b%n%d%n%d%n%d%n%f%n%f%n", isPassed, count, marks, studentsCount, pi, secondPi);
+
+    }
+}
